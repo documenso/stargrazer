43a09ba0e97ccec0adc08b0004ecd49b529ee254
Timur Ercan <timur.ercan31@gmail.com>
2023-08-12 18:15:00 +0200

    helth endpoint

---
+++ pages/api/health.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+import { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "../../prisma/prisma";
+
+export default async function getHandler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    const stats = await prisma.gitHubStats.findMany();
+
+    return res.status(200).send("API healthy :)");
+  } catch (error: any) {
+    console.error("Error fetching repository info:", error.message);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+}
